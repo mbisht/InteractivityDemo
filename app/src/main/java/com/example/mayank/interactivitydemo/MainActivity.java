12ddf4c5f26b9afae12296cb6b3e0c422e74376b
mbisht <mayankbisht.com@gmail.com>
2018-09-30 17:12:29 +0530

    final commit

---
+++ app/src/main/java/com/example/mayank/interactivitydemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mayank.interactivitydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+
+        EditText  nameEditText =  (EditText) findViewById(R.id.nameEditText);
+
+        Log.i("Info", "It Worked!!");
+
+        Log.i("Values", nameEditText.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
